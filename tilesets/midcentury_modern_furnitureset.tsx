fc76c208f83988f91849636af5c22a06d3d2b42e
Ester Pinheiro <ester.copinheiro@gmail.com>
2026-01-06 09:58:48 -0300

    Primeiro upload do mapa

---
+++ tilesets/midcentury_modern_furnitureset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="midcentury_modern_furnitureset" tilewidth="32" tileheight="32" tilecount="270" columns="18">
+ <image source="../../../Learn Tiles/midcentury_modern_furnitureset.png" width="576" height="480"/>
+</tileset>
